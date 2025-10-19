df7daef4ba8dea09ba3df7549c7493b867a52585
Chengiz Khan <0xmoeedkhan@gmail.com>
2025-10-19 17:33:28 +0500

    jarvis main.py updated

--- main.py
+++ main.py
@@ -11,3 +11,19 @@ def speak(text):
 
 if __name__ == "__main__":
     speak("How may I be of service?")
+
+    # obtain audio from the microphone
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Listening...")
+        audio = r.listen(source, timeout=3)
+
+    print("Recognizing...")
+    # recognize speech using Google Speech Recognition
+    try:
+        command = r.recognize_google(audio)
+        print(command)
+    except sr.UnknownValueError:
+        print("Google Speech Recognition could not understand audio")
+    except sr.RequestError as e:
+        print("Could not request results from Google Speech Recognition service; {0}".format(e))
