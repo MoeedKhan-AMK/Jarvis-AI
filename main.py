e9eef3292f57942848ee551728f0b59bac0329aa
chengiz khan <0xmoeedkhan@gmail.com>
2025-10-20 18:57:47 +0500

    Old main.py deleted. New Main.py Updated

--- main.py
+++ main.py
@@ -1,35 +1,31 @@
-import speech_recognition as sr  #speech_recognition is a library in Python used for recognizing speech input
-import webbrowser       #webbrowser is a standard library in Python used to open web pages in the default browser
-import pyttsx3      #pyttsx3 is a text-to-speech conversion library in Python
-import time        #time is a standard library in Python used for time-related functions
+import speech_recognition as sr
+import webbrowser
+import pyttsx3
+import time
 
-recognizer = sr.Recognizer()            #Recognizer is an instance of the Recognizer class from the speech_recognition library
-engine = pyttsx3.init()            #engine is an instance of the pyttsx3 class for text-to-speech conversion
+recognizer = sr.Recognizer()
+engine = pyttsx3.init()
 
 
 def speak(text):
-        engine.say(text)
-        engine.runAndWait()
+    engine.say(text)
+    engine.runAndWait()
 
 if __name__ == "__main__":
-        speak("Initializing Jarvis")
-        # time.sleep(1)
-        while True:
-            # obtain audio from the microphone
-            r = sr.Recognizer()
-            with sr.Microphone(device_index=0) as source:
+    speak("Jarvis is initializing...")
+
+    while True:
+        r = sr.Recognizer()
+        print("Recognizing...")
+
+        try:
+            with sr.Microphone() as source:
                 print("Listening...")
-                r.adjust_for_ambient_noise(source, duration=0.5)
-                audio = r.listen(source, timeout=5, phrase_time_limit=5)
-
-            print("Recognizing...")
-            # recognize speech using Google Speech Recognition
-            try:
-                command = r.recognize_google(audio)
-                print(command)
-            except sr.UnknownValueError:
-                print("Google Speech Recognition could not understand audio")
-            except sr.RequestError as e:
-                print("Could not request results from Google Speech Recognition service; {0}".format(e))
-
-
+                audio = r.listen(source)
+            command = r.recognize_google(audio)
+            print(command)
+        except sr.UnknownValueError:
+            print("Google Speech Recognition could not understand audio")
+        except sr.RequestError as e:
+            print("Could not request results from Google Speech Recognition service; {0}".format(e))
+        
