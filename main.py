156440191ad9d440bf620eed3529ea5135f23e11
chengiz khan <0xmoeedkhan@gmail.com>
2025-10-23 18:41:57 +0500

    Wake Work successful

--- main.py
+++ main.py
@@ -35,13 +35,12 @@ if __name__ == "__main__":
                 time.sleep(0.5) # Short delay before next command
                 
                 
-                with sr.Microphone(device_index=0) as source:
-                    print("Jarvis Activated!")
-                    audio = r.listen(source)
-                    command = r.recognize_google(audio)
-                    
-                    
-                    process_command(command)
+            with sr.Microphone(device_index=0) as source:
+                print("Jarvis Activated!")
+                audio = r.listen(source)
+                command = r.recognize_google(audio)
+                
+                process_command(command)
 
 
         except sr.UnknownValueError:
