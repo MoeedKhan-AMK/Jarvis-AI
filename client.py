51947096726f0d795dd39401ef996d15c4a2a1c3
chengiz khan <0xmoeedkhan@gmail.com>
2025-10-27 20:31:12 +0500

    API Successfully Answering

--- client.py
+++ client.py
@@ -4,12 +4,11 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-client = genai.Client(api_key=os.getenv("GOOGLE_API_KEY"))
+client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
 
 response = client.models.generate_content(
-    model="gemini-2.5-flash", 
-    contents="Explain how AI works in a few words"
+    model="gemini-2.5-flash",
+    contents="Price of Bitcoin?"
 )
-
 print(response.text)
 
